1e49cc8f2ff1768a0ba52391d690b405e9b2c357
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-11-24 15:12:46 +0100

    camera first step

--- src/common/Camera.cpp
+++ src/common/Camera.cpp
@@ -33,10 +33,10 @@ void Camera::setPosition(int position)
 int Camera::configure()
 {
 
+  namedWindow(this->main_window, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_NORMAL);
   Mat input, original;
 
-  CvCapture *camCapture = this->camera_handle;
-  int ret = 0;
+  CvCapture *camCapture = NULL;
   bool configuring = true;
 
   // camera init
@@ -44,7 +44,28 @@ int Camera::configure()
     std::cout << "LOG: Failed to capture from camera\n";
     return -1;
   }
-  std::cout << "LOG: Everything is fine\n";
+
+  std::cout << "LOG: Camera opened successfully << " << camCapture << "\n";
+
+  IplImage *cameraFrame;
+
+  // first step starts here
+  while (configuring)
+  {
+    if ((cameraFrame = cvQueryFrame(camCapture)))
+    {
+      input = Mat(cameraFrame, false);
+      imshow(main_window, input);
+    }
+
+    if (cvWaitKey(30) != -1)
+    {
+      configuring = false;
+    }
+  }
+  cvReleaseCapture(&camCapture);
+
+  cvDestroyAllWindows();
 
   this->setPosition(11);
 
